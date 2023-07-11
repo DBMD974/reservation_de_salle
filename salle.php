5c2d7a72c36edd3b6c1cf5ccfeec44997f5d3924
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-11 08:55:48 +0400

    element du dossier

---
+++ salle.php
@@ -0,0 +1,25 @@
+<?php include ('Maconnexion.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réservation de Salle</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+
+
+
+
+
+
+
+
+
+<script src="script.js"></script>
+</body>
+</html>
