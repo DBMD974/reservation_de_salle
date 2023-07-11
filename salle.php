1140951c9a18175e0bb2728ed9dfa6539a15b034
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-11 11:18:32 +0400

    commit 'perfectionniste'

--- salle.php
+++ salle.php
@@ -26,6 +26,41 @@
   </div>
 </nav>
 
+<br>
+
+<!-- mon carousel -->
+<div class="carousel">
+  <div class="carousel-item">
+    <img src="img/istockphoto-478919558-612x612.jpg" alt="Image 1">
+    <div class="carousel-text">
+      <h3>Choisissez la table de Reunion</h3>
+      <p>Qui vous conviennes</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="img/istockphoto-105869596-612x612.jpg" alt="Image 2">
+    <div class="carousel-text">
+      <h3>Description 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="img/istockphoto-911295362-612x612.jpg" alt="Image 3">
+    <div class="carousel-text">
+      <h3>Description 3</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="img/istockphoto-480155820-612x612.jpg" alt="Image 4">
+    <div class="carousel-text">
+      <h3>Description 4</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+</div>
+
+
 
 
 
