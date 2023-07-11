9f5d9e721b5c8d42856fd5a67fa3bb1fc270abfb
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-11 10:17:51 +0400

    de nouveau element apporter

--- salle.php
+++ salle.php
@@ -9,6 +9,73 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<header id="header">
+        <nav id="navbar" class="navbar">
+          <div class="logo">
+            <img src="" alt="Logo">
+          </div>
+        
+          <ul class="navbar-links">
+            <li class="dropdown">
+              <a href="#">Pages d'accueil</a>
+              <div class="dropdown-content">
+                <a href="#">Sous-page 1</a>
+                <a href="#">Sous-page 2</a>
+                <a href="#">Sous-page 3</a>
+              </div>
+            </li>
+        
+            <li class="dropdown">
+              <a href="#">À Propos de nous</a>
+              <div class="dropdown-content">
+                <a href="#">Sous-page 1</a>
+                <a href="#">Sous-page 2</a>
+        
+            </li>
+            </div>
+        
+            <li class="dropdown">
+              <a href="#">Salles et projets</a>
+              <div class="dropdown-content">
+                <a href="#">Salle basic</a>
+                <a href="#">Salle medium</a>
+                <a href="#">G</a>
+                <a href="#">Sous-page 3</a>
+                <a href="#">Projet 1</a>
+                <a href="#">Projet 2</a>
+                <a href="#">Projet 3</a>
+                <a href="#">Sous-page 3</a>
+              </div>
+            </li>
+        
+            <li class="dropdown">
+              <a href="#">Blog</a>
+              <div class="dropdown-content">
+                <a href="#">Projet 1</a>
+                <a href="#">Projet 2</a>
+                <a href="#">Projet 3</a>
+                <a href="#">Projet 3</a>
+              </div>
+            </li>
+          </ul>
+        
+          <a class="contact-btn" href="#">Contact</a>
+        </nav>
+<!-- pour le message dans le header -->
+
+<div class="header-content">
+    <img class="logo" src="img/icon05.png" alt="Logo">
+    <h3>Accueillir</h3>
+    <h2>au paradis de la beauté</h2>
+    <p>Dans l'édition et la conception graphique, le <br>
+       lorem ipsum est couramment utilisé pour <br>
+        pleurer</p>
+    <a class="reserve-button" href="#">Réserver un massage</a>
+  </div>
+
+
+      </header>
     
 
 
